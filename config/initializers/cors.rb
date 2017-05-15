dea9ef84002d94dbe68f02d068c4b11cbd6f7c54
025609 <matthewnguyen@C1MRQ046HF4H.fios-router.home>
2017-05-15 16:32:37 -0400

    change cord.rb file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,10 +8,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'localhost:8000'
-
     resource '*',
       headers: :any,
-      methods: [:get,:options, :head]
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
 
